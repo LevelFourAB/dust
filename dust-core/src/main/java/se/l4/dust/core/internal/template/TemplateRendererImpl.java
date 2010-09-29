307a67ad07e8b8127a75f5e5ff20a7a42a2b4373
Andreas Holstenson <a@holstenson.se>
2010-09-29 16:32:05 +0200

    @Inject needed

--- dust-core/src/main/java/se/l4/dust/core/internal/template/TemplateRendererImpl.java
+++ dust-core/src/main/java/se/l4/dust/core/internal/template/TemplateRendererImpl.java
@@ -5,6 +5,8 @@ import java.net.URL;
 import org.jdom.Document;
 import org.jdom.JDOMException;
 
+import com.google.inject.Inject;
+
 import se.l4.dust.api.template.TemplateRenderer;
 import se.l4.dust.core.internal.template.dom.TemplateEmitter;
 import se.l4.dust.core.template.TemplateCache;
@@ -15,6 +17,7 @@ public class TemplateRendererImpl
 	private final TemplateEmitter emitter;
 	private final TemplateCache cache;
 
+	@Inject
 	public TemplateRendererImpl(TemplateEmitter emitter, TemplateCache cache)
 	{
 		this.emitter = emitter;
