0e1ca8dda92c23cc6e23360af15a10a1b29d9b20
Andreas Holstenson <a@holstenson.se>
2011-07-25 13:54:03 +0200

    Removed unused class

--- dust-core/src/main/java/se/l4/dust/api/template/spi/DynamicContentEvaluator.java
+++
@@ -1,8 +0,0 @@
-package se.l4.dust.api.template.spi;
-
-import se.l4.dust.api.template.dom.DynamicContent;
-
-public interface DynamicContentEvaluator
-{
-	String evaluate(DynamicContent content);
-}
