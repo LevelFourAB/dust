0c517180b1bbeeace757fd3cfd10b40be59a8e9d
Andreas Holstenson <a@holstenson.se>
2011-08-04 10:27:21 +0200

    Support for looping over Iterable and Iterator

--- dust-core/src/main/java/se/l4/dust/core/internal/template/components/LoopComponent.java
+++ dust-core/src/main/java/se/l4/dust/core/internal/template/components/LoopComponent.java
@@ -1,12 +1,16 @@
 package se.l4.dust.core.internal.template.components;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 import se.l4.dust.api.TemplateException;
+import se.l4.dust.api.conversion.TypeConverter;
 import se.l4.dust.api.template.RenderingContext;
 import se.l4.dust.api.template.dom.Content;
 import se.l4.dust.api.template.spi.TemplateOutputStream;
 import se.l4.dust.core.internal.template.dom.Emitter;
+
+import com.google.inject.Inject;
 
 
 /**
@@ -18,15 +22,20 @@ import se.l4.dust.core.internal.template.dom.Emitter;
 public class LoopComponent
 	extends EmittableComponent
 {
-	public LoopComponent()
+	private final TypeConverter converter;
+
+	@Inject
+	public LoopComponent(TypeConverter converter)
 	{
 		super("loop", LoopComponent.class);
+		
+		this.converter = converter;
 	}
 	
 	@Override
 	public Content copy()
 	{
-		return new LoopComponent().copyAttributes(this);
+		return new LoopComponent(converter).copyAttributes(this);
 	}
 	
 	public Attribute getSource()
@@ -60,17 +69,55 @@ public class LoopComponent
 		if(value == null) throw new TemplateException("Attribute value is required");
 		
 		Object sourceData = source.getValue(ctx, data);
-		// TODO: Use conversions
-		Iterable<Object> items = (Iterable) sourceData;
-		
-		for(Object o : items)
+		if(! (sourceData instanceof Iterable || sourceData instanceof Iterator))
 		{
-			value.setValue(ctx, data, o);
-			
-			for(Content c : getRawContents())
+			// Try to convert to either Iterable or Iterator
+			if(converter.canConvertBetween(sourceData, Iterable.class))
 			{
-				emitter.emit(ctx, out, data, this, lastData, c);
+				sourceData = converter.convert(sourceData, Iterable.class);
 			}
+			else if(converter.canConvertBetween(sourceData, Iterator.class))
+			{
+				sourceData = converter.convert(sourceData, Iterator.class);
+			}
+			else
+			{
+				throw new TemplateException("Unable to convert " + sourceData + " to either Iterable or Iterator");
+			}
+		}
+		
+		if(sourceData instanceof Iterable)
+		{
+			Iterable<Object> items = (Iterable) sourceData;
+			
+			for(Object o : items)
+			{
+				emitLoopContents(emitter, ctx, out, data, lastData, value, o);
+			}
+		}
+		else if(sourceData instanceof Iterator)
+		{
+			Iterator<Object> it = (Iterator) sourceData;
+			
+			while(it.hasNext())
+			{
+				Object o = it.next();
+				
+				emitLoopContents(emitter, ctx, out, data, lastData, value, o);
+			}
+		}
+	}
+
+	private void emitLoopContents(Emitter emitter, RenderingContext ctx,
+			TemplateOutputStream out, Object data, Object lastData,
+			Attribute value, Object o)
+		throws IOException
+	{
+		value.setValue(ctx, data, o);
+		
+		for(Content c : getRawContents())
+		{
+			emitter.emit(ctx, out, data, this, lastData, c);
 		}
 	}
 	
