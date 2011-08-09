5c6b89cd3155881f38c60448d2c9ea920e959e89
Andreas Holstenson <a@holstenson.se>
2011-08-09 09:14:32 +0200

    Modified MemoryResource constructors

--- dust-core/src/main/java/se/l4/dust/api/resource/MemoryResource.java
+++ dust-core/src/main/java/se/l4/dust/api/resource/MemoryResource.java
@@ -1,9 +1,10 @@
 package se.l4.dust.api.resource;
 
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+
+import com.google.common.io.ByteStreams;
 
 /**
  * Implementation of {@link Resource} that is stored entirely in memory.
@@ -22,27 +23,16 @@ public class MemoryResource
 	public MemoryResource(String contentType, String contentEncoding, InputStream stream)
 		throws IOException
 	{
-		this.contentType = contentType;
-		this.contentEncoding = contentEncoding;
-		
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		byte[] buf = new byte[1024];
-		int len;
-		while((len = stream.read(buf)) != -1)
-		{
-			out.write(buf, 0, len);
-		}
-		
-		this.data = out.toByteArray();
-		lastModified = System.currentTimeMillis();
+		this(contentType, contentEncoding, ByteStreams.toByteArray(stream));
 	}
 	
 	public MemoryResource(String contentType, String contentEncoding, byte[] data)
+		throws IOException
 	{
 		this.contentType = contentType;
 		this.contentEncoding = contentEncoding;
-		this.data = data;
 		
+		this.data = data;
 		lastModified = System.currentTimeMillis();
 	}
 	
