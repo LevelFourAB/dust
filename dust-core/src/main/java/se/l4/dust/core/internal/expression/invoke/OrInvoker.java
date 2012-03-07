513ca60144ff1a6f9eea143b6bc65a566c4d9a54
Andreas Holstenson <a@holstenson.se>
2012-03-07 12:54:13 +0100

    Fixed a bug with OR expressions in dev mode

--- dust-core/src/main/java/se/l4/dust/core/internal/expression/invoke/OrInvoker.java
+++ dust-core/src/main/java/se/l4/dust/core/internal/expression/invoke/OrInvoker.java
@@ -1,5 +1,6 @@
 package se.l4.dust.core.internal.expression.invoke;
 
+import se.l4.dust.api.Context;
 import se.l4.dust.core.internal.expression.ErrorHandler;
 import se.l4.dust.core.internal.expression.ExpressionCompiler;
 import se.l4.dust.core.internal.expression.ast.Node;
@@ -25,6 +26,27 @@ public class OrInvoker
 		Boolean lb = castLeftNotNull(errors, left);
 		Boolean rb = castLeftNotNull(errors, right);
 		return lb || rb;
+	}
+	
+	@Override
+	public Object get(ErrorHandler errors, Context context, Object root, Object instance)
+	{
+		try
+		{
+			Object lv = left.get(errors, context, root, instance);
+			Boolean lb = castLeftNotNull(errors, lv);
+			if(lb)
+			{
+				return true;
+			}
+			
+			Object rv = right.get(errors, context, root, instance);
+			return check(errors, lv, rv);
+		}
+		catch(Throwable t)
+		{
+			throw errors.error(node, t);
+		}
 	}
 	
 	@Override
