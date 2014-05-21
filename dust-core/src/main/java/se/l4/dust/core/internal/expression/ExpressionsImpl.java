95312b916fe506fbc83fba8320820cc4609dfdbb
Andreas Holstenson <a@holstenson.se>
2014-05-21 08:05:48 +0200

    Caching of expressions

--- dust-core/src/main/java/se/l4/dust/core/internal/expression/ExpressionsImpl.java
+++ dust-core/src/main/java/se/l4/dust/core/internal/expression/ExpressionsImpl.java
@@ -15,6 +15,8 @@ import se.l4.dust.core.internal.expression.ast.Node;
 import se.l4.dust.core.internal.expression.invoke.Invoker;
 import se.l4.dust.core.internal.template.dom.ExpressionContent;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.google.inject.Stage;
@@ -32,6 +34,7 @@ public class ExpressionsImpl
 	private final TypeConverter converter;
 	private final ConcurrentMap<String, ExpressionSource> sources;
 	private final boolean production;
+	private final Cache<Key, Expression> cachedExpressions;
 	
 	@Inject
 	public ExpressionsImpl(TypeConverter converter, Stage stage)
@@ -39,6 +42,9 @@ public class ExpressionsImpl
 		this.converter = converter;
 		production = stage != Stage.DEVELOPMENT;
 		sources = new ConcurrentHashMap<String, ExpressionSource>();
+		
+		cachedExpressions = CacheBuilder.newBuilder()
+			.build();
 	}
 
 	@Override
@@ -103,6 +109,13 @@ public class ExpressionsImpl
 	@Override
 	public Expression compile(URL source, Map<String, String> namespaces, String expression, Class<?> localContext)
 	{
+		Key key = new Key(expression, localContext);
+		Expression result = cachedExpressions.getIfPresent(key);
+		if(result != null)
+		{
+			return result;
+		}
+		
 		if(production)
 		{
 			ErrorHandler errors = new ErrorHandlerImpl(expression);
@@ -117,11 +130,65 @@ public class ExpressionsImpl
 			).resolve(localContext);
 			
 			ExpressionCompiler compiler = new ExpressionCompiler(errors, expression, localContext, invoker);
-			return compiler.compile();
+			result =  compiler.compile();
 		}
 		else
 		{
-			return new ExpressionDebugger(converter, this, source, namespaces, expression, localContext);
+			result = new ExpressionDebugger(converter, this, source, namespaces, expression, localContext);
+		}
+		
+		cachedExpressions.put(key, result);
+		return result;
+	}
+	
+	private static class Key
+	{
+		private final String expression;
+		private final Class<?> context;
+		
+		public Key(String expression, Class<?> context)
+		{
+			this.expression = expression;
+			this.context = context;
+		}
+
+		@Override
+		public int hashCode()
+		{
+			final int prime = 31;
+			int result = 1;
+			result = prime * result
+					+ ((context == null) ? 0 : context.hashCode());
+			result = prime * result
+					+ ((expression == null) ? 0 : expression.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj)
+		{
+			if(this == obj)
+				return true;
+			if(obj == null)
+				return false;
+			if(getClass() != obj.getClass())
+				return false;
+			Key other = (Key) obj;
+			if(context == null)
+			{
+				if(other.context != null)
+					return false;
+			}
+			else if(!context.equals(other.context))
+				return false;
+			if(expression == null)
+			{
+				if(other.expression != null)
+					return false;
+			}
+			else if(!expression.equals(other.expression))
+				return false;
+			return true;
 		}
 	}
 }
